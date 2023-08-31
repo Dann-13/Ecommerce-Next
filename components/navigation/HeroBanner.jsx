4bf9b10f031662a121be95cbb19a86cab1a944c0
Dann-13 <dannyelgomezz.13@gmail.com>
2023-08-30 23:19:24 -0500

    Mostrando Banner con producto

--- components/navigation/HeroBanner.jsx
+++ components/navigation/HeroBanner.jsx
@@ -1,24 +1,28 @@
-
 import React from 'react';
 import Link from 'next/link';
-export function Herobanner() {
+import { urlFor } from '../../lib/client';
+
+export function Herobanner({ heroBanner }) {
+  const imageUrl = urlFor(heroBanner.image[0].asset._ref).url(); // Genera la URL de la imagen
+
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src='' alt='Audifonos' className='hero-banner-image' />
+        <p className='beats-solo'>{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={imageUrl} alt="headphones" className='hero-banner-image' />
 
         <div>
           <Link href='/product/ID'>
             <button type='button'> Button Text</button>
           </Link>
           <div className='desc'>
-              <h5>Description</h5>
-              <p>Description</p>
+            <h5>Description</h5>
+            <p>Description</p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
