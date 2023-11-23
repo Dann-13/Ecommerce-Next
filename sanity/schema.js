d7073d79056ce9898e40f799e0557314d970c910
Dann-13 <dannyelgomezz.13@gmail.com>
2023-11-23 18:07:12 -0500

    Quitar carpetas de la caché

--- sanity/schema.js
+++
@@ -1,7 +0,0 @@
-import product from './schemas/product';
-import banner from './schemas/banner';
-import carousel from './schemas/carousel';
-
-export const schema = {
-  types: [product, banner, carousel],
-}
